ad256ae61ffa0b3702e5d754bf5fc807721da286
stez-mind <stefano.zambon@mindmusiclabs.com>
2016-04-07 12:22:46 +0200

    Refactoring: use type instead than tag in Command Messages

--- linux/src/message/base_command.h
+++ linux/src/message/base_command.h
@@ -17,7 +17,7 @@ namespace sensei {
 
 class MessageFactory;
 
-enum class CommandTag;
+enum class CommandType;
 
 /**
  * @brief Abstract base class for commands.
@@ -49,9 +49,9 @@ public:
      * @brief Tag type for RTTI-like dynamic dispatch.
      *        Tag definitions are defined in messages/command_defs.h
      */
-    CommandTag tag()
+    CommandType type()
     {
-        return _tag;
+        return _type;
     }
 
     /**
@@ -63,7 +63,7 @@ public:
     {
         uint64_t result = 0u;
         result |= static_cast<uint64_t>(_sensor_index) << 48;
-        result |= static_cast<uint64_t>(_tag) << 32;
+        result |= static_cast<uint64_t>(_type) << 32;
         result |= static_cast<uint64_t>(_timestamp);
         return result;
     }
@@ -71,23 +71,23 @@ public:
 protected:
     Command(const int sensor_index,
             bool is_external,
-            CommandTag tag,
+            CommandType type,
             const uint32_t timestamp=0) :
                 BaseMessage(sensor_index, timestamp),
                 _is_external(is_external),
-                _tag(tag)
+                _type(type)
     {
     }
 
     bool    _is_external;
-    CommandTag _tag;
+    CommandType _type;
 };
 
 //////////////////////////////////////////////////////////////////////////////////
 // Concrete command classes
 //////////////////////////////////////////////////////////////////////////////////
 
-#define __SENSEI_DECLARE_MESSAGE(ClassName, command_tag, InternalType, representation_prefix, is_external) \
+#define __SENSEI_DECLARE_COMMAND(ClassName, command_type, InternalType, representation_prefix, is_external) \
 class ClassName : public Command \
 { \
 public: \
@@ -104,18 +104,18 @@ private:\
     ClassName(const int sensor_index,\
               const InternalType data,\
               const uint32_t timestamp=0) :\
-        Command(sensor_index, is_external, command_tag, timestamp),\
+        Command(sensor_index, is_external, command_type, timestamp),\
         _data(data)\
     {\
     }\
     InternalType _data;\
 }
 
-#define SENSEI_DECLARE_EXTERNAL_MESSAGE(ClassName, command_tag, InternalType, representation_prefix) \
-    __SENSEI_DECLARE_MESSAGE(ClassName, command_tag, InternalType, representation_prefix, true)
+#define SENSEI_DECLARE_EXTERNAL_COMMAND(ClassName, command_type, InternalType, representation_prefix) \
+    __SENSEI_DECLARE_COMMAND(ClassName, command_type, InternalType, representation_prefix, true)
 
-#define SENSEI_DECLARE_INTERNAL_MESSAGE(ClassName, InternalType, representation_prefix) \
-    __SENSEI_DECLARE_MESSAGE(ClassName, CommandTag::INTERNAL, InternalType, representation_prefix, false)
+#define SENSEI_DECLARE_INTERNAL_COMMAND(ClassName, command_type, InternalType, representation_prefix) \
+    __SENSEI_DECLARE_COMMAND(ClassName, command_type, InternalType, representation_prefix, false)
 
 }; // namespace sensei
 
--- linux/src/message/command_defs.h
+++ linux/src/message/command_defs.h
@@ -17,6 +17,9 @@
 #ifndef SENSEI_COMMAND_DEFS_H
 #define SENSEI_COMMAND_DEFS_H
 
+#include <memory>
+#include <vector>
+
 #include "base_command.h"
 
 namespace sensei {
@@ -24,9 +27,9 @@ namespace sensei {
 /**
  * @brief Tags used for RTTI emulation.
  */
-enum class CommandTag
+enum class CommandType
 {
-    INTERNAL,
+    // External Commands
     SET_SAMPLING_RATE,
     SET_PIN_TYPE,
     SET_ENABLED,
@@ -37,6 +40,10 @@ enum class CommandTag
     SET_SLIDER_MODE_ENABLED,
     SET_SLIDER_THRESHOLD,
     SEND_DIGITAL_PIN_VALUE,
+    // Internal Commands
+    SET_INVERT_ENABLED,
+    SET_INPUT_SCALE_RANGE_LOW,
+    SET_INPUT_SCALE_RANGE_HIGH,
     N_COMMAND_TAGS
 };
 
@@ -45,7 +52,6 @@ enum class CommandTag
  */
 enum class PinType
 {
-    DISABLED,
     DIGITAL_INPUT,
     DIGITAL_OUTPUT,
     ANALOG_INPUT,
@@ -64,59 +70,97 @@ enum class SendingMode
     N_SENDING_MODES
 };
 
+enum class CommandErrorCode
+{
+    OK,
+    UNHANDLED_COMMAND_FOR_SENSOR_TYPE,
+    INVALID_RANGE,
+    CLIP_WARNING,
+    N_COMMAND_ERROR_CODES
+};
+
 ////////////////////////////////////////////////////////////////////////////////
 // Concrete command subclasses definitions
 ////////////////////////////////////////////////////////////////////////////////
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetSamplingRateCommand,
-                                CommandTag::SET_SAMPLING_RATE,
+// External commands
+
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetSamplingRateCommand,
+                                CommandType::SET_SAMPLING_RATE,
                                 float,
                                 "Set Sampling Rate");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetEnabledCommand,
-                                CommandTag::SET_ENABLED,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetEnabledCommand,
+                                CommandType::SET_ENABLED,
                                 bool,
                                 "Set Enabled");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetPinTypeCommand,
-                                CommandTag::SET_PIN_TYPE,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetPinTypeCommand,
+                                CommandType::SET_PIN_TYPE,
                                 PinType,
                                 "Set Pin Type");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetSendingModeCommand,
-                                CommandTag::SET_SENDING_MODE,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetSendingModeCommand,
+                                CommandType::SET_SENDING_MODE,
                                 SendingMode ,
                                 "Set Sending Mode");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetSendingDeltaTicksCommand,
-                                CommandTag::SET_SENDING_DELTA_TICKS,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetSendingDeltaTicksCommand,
+                                CommandType::SET_SENDING_DELTA_TICKS,
                                 int,
                                 "Set Sending Delta Ticks");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetADCBitResolutionCommand,
-                                CommandTag::SET_ADC_BIT_RESOLUTION,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetADCBitResolutionCommand,
+                                CommandType::SET_ADC_BIT_RESOLUTION,
                                 int,
                                 "Set ADC Bit Resolution");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetLowpassCutoffCommand,
-                                CommandTag::SET_LOWPASS_CUTOFF,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetLowpassCutoffCommand,
+                                CommandType::SET_LOWPASS_CUTOFF,
                                 float,
                                 "Set Lowpass Cutoff");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetSliderModeEnabledCommand,
-                                CommandTag::SET_SLIDER_MODE_ENABLED,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetSliderModeEnabledCommand,
+                                CommandType::SET_SLIDER_MODE_ENABLED,
                                 bool,
                                 "Set Slider Mode Enabled");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SetSliderThresholdCommand,
-                                CommandTag::SET_SLIDER_THRESHOLD,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SetSliderThresholdCommand,
+                                CommandType::SET_SLIDER_THRESHOLD,
                                 int,
                                 "Set Slider Threshold");
 
-SENSEI_DECLARE_EXTERNAL_MESSAGE(SendDigitalPinValueCommand,
-                                CommandTag::SEND_DIGITAL_PIN_VALUE,
+SENSEI_DECLARE_EXTERNAL_COMMAND(SendDigitalPinValueCommand,
+                                CommandType::SEND_DIGITAL_PIN_VALUE,
                                 bool,
                                 "Send Digital Pin Value");
+
+// Internal commands
+
+SENSEI_DECLARE_INTERNAL_COMMAND(SetInvertEnabledCommand,
+                                CommandType::SET_INVERT_ENABLED,
+                                bool,
+                                "Set Invert Enabled");
+
+SENSEI_DECLARE_INTERNAL_COMMAND(SetInputScaleRangeLow,
+                                CommandType::SET_INPUT_SCALE_RANGE_LOW,
+                                int,
+                                "Set Input Scale Range Low");
+
+SENSEI_DECLARE_INTERNAL_COMMAND(SetInputScaleRangeHigh,
+                                CommandType::SET_INPUT_SCALE_RANGE_HIGH,
+                                int,
+                                "Set Input Scale Range Low");
+
+////////////////////////////////////////////////////////////////////////////////
+// Containers
+////////////////////////////////////////////////////////////////////////////////
+
+// these are used in some internal interfaces that operates on list of pointers to commands
+
+typedef std::vector<std::unique_ptr<Command>> CommandContainer;
+
+typedef std::back_insert_iterator<CommandContainer> CommandIterator;
 
 }; // namespace sensei
 
