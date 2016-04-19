a48af2dfedd5f461ca91702972ed604c8ebdfd43
Simone Minto <simone.minto@me.com>
2016-04-19 05:22:24 +0200

    Reordered members position

--- teensy/SW_SENSEI/manage_io.h
+++ teensy/SW_SENSEI/manage_io.h
@@ -17,14 +17,12 @@ class ManageIO{
     ManageIO();
 	~ManageIO();
 
-    int32_t setSystem(uint16_t _nPin, uint16_t _nDigitalPin);
-
     ManageIMU imu;
 
+    int32_t setSystem(uint16_t _nPin, uint16_t _nDigitalPin);
     int32_t setPinValue(uint16_t _idxPin, uint16_t _value);
     int32_t getPinValue(uint16_t _idxPin, uint16_t &_value);
     uint16_t getPinValue(uint16_t _idxPin);
-
 	int32_t configurePin(ePinType type, SetupPin* setupPin);
 	uint8_t getPinType(uint16_t _idxPin);
     bool isPinValueChanged(uint16_t _idxPin);
@@ -32,7 +30,6 @@ class ManageIO{
 	int32_t setDigitalBank(uint16_t _idxBank, uint8_t value);
 	int32_t setDigitalPin(uint16_t _idxPin, bool _value);
     uint8_t getNmultiplexer();
-
 	uint16_t getNumberOfPins();
 	uint16_t getNumberOfDigitalPins();
     bool isSystemInitialized();
@@ -41,9 +38,6 @@ class ManageIO{
   private:
 
      std::vector<PIN*> vPin;
-
-     void setDigitalPin();
-
      uint8_t nMul;
 	 uint16_t nPin;
 	 uint16_t nDigitalPin;
@@ -51,6 +45,9 @@ class ManageIO{
 
 	 uint8_t* vBankDigitalPin;
      bool systemInitialized;
+
+     void setDigitalPin();
+
 };
 
 #endif // MANAGEIO_H
