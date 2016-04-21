281f183403caeddb098091eef0d9e2e491b3a751
Simone Minto <simone.minto@me.com>
2016-04-21 19:26:28 +0200

    Added debug comment

--- teensy/SW_SENSEI/digital_input.cpp
+++ teensy/SW_SENSEI/digital_input.cpp
@@ -2,9 +2,8 @@
 
 DigitalInput::DigitalInput(SetupPin* setupPin) : PIN(setupPin)
 {
+    //SerialDebug.println("DigitalInput");
 	_type = ePinType::PIN_DIGITAL_INPUT;
-	_sendingMode = setupPin->sendingMode;
-	_deltaTicksContinuousMode = setupPin->deltaTicksContinuousMode;
 }
 
 DigitalInput::~DigitalInput()
