ca0e8ce00439da0d7b0ad9aee1dfdc64d1233e2f
Gustav Andersson <gustav.andersson@mindmusiclabs.com>
2018-03-06 15:22:22 +0100

    Raspamockup tool

---
+++ linux/test/tools/socket_example/raspa_mockup.cpp
@@ -0,0 +1,176 @@
+
+
+#include <iostream>
+#include <cstring>
+#include <thread>
+#include <cassert>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <cerrno>
+#include <unistd.h>
+#include <csignal>
+
+
+constexpr char SENSEI_SOCKET[] = "/tmp/sensei";
+constexpr char RASPA_SOCKET[] = "/tmp/raspa";
+constexpr int  SOCKET_TIMEOUT_S = 2;
+constexpr auto PING_INTERVAL = std::chrono::seconds(2);
+
+/* Dummy task that can act as a stand in for raspalib
+ * It claims a socket, prints what it receives on it
+ * and sends some data to sensei if available 
+ * 
+ * build cmd: 
+ * g++ raspa_mockup.cpp -g -o mockup -lpthread
+ */
+
+
+
+struct RaspaPacket
+{
+    char data[20];
+};
+
+class RaspaMockup
+{
+public:
+    RaspaMockup() 
+    {
+        _in_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+        _out_socket = socket(AF_UNIX, SOCK_DGRAM, 0);
+        sockaddr_un address;
+        address.sun_family = AF_UNIX;
+        strcpy(address.sun_path, RASPA_SOCKET);
+        /* In case we didn't quit gracefully, clear the socket handle */
+        unlink(RASPA_SOCKET);
+        auto res = bind(_in_socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_un));
+        assert(res == 0);
+
+        timeval time;
+        time.tv_usec = 0;
+        time.tv_sec = SOCKET_TIMEOUT_S;
+        res = setsockopt(_in_socket, SOL_SOCKET, SO_RCVTIMEO, &time, sizeof(time));
+        std::cout << "Set recv timeout: "  << strerror(errno) << std::endl;
+
+        assert(res == 0);
+        std::cout << "Started Raspa Mockup"  << std::endl;
+    }
+
+    ~RaspaMockup()
+    {
+        stop();
+        unlink(RASPA_SOCKET);
+    }
+    
+    void run() 
+    {
+        if (!_running)
+        {
+            _running = true;
+            _read_thread = std::thread(&RaspaMockup::read_loop, this);
+            _write_thread = std::thread(&RaspaMockup::write_loop, this);
+        }
+    }
+    void stop() 
+    {
+        if(_running)
+        {
+            std::cout << "Stopping " << std::endl;
+            _running = false;
+            _read_thread.join();
+            _write_thread.join();
+        }
+    }
+
+private:
+    void read_loop()
+    {
+        RaspaPacket buffer;
+        while (_running)
+        {
+            memset(&buffer, 0, sizeof(buffer));
+            auto bytes = recv(_in_socket, &buffer, sizeof(buffer), 0);
+            if (bytes >= sizeof(RaspaPacket))
+            {
+                std::cout << "Received: " << buffer.data << std::endl;
+                if (!_connected)
+                {
+                    /* Try to connect to sensei, it should be up and running now */
+                    sockaddr_un address;
+                    address.sun_family = AF_UNIX;
+                    strcpy(address.sun_path, SENSEI_SOCKET);
+                    auto res = connect(_out_socket, reinterpret_cast<sockaddr*>(&address), sizeof(sockaddr_un));
+                    if (res == 0)
+                    {
+                        timeval time;
+                        time.tv_usec = 0;
+                        time.tv_sec = SOCKET_TIMEOUT_S;
+                        auto res = setsockopt(_out_socket, SOL_SOCKET, SO_SNDTIMEO, &time, sizeof(time));
+                        assert(res == 0);
+                        std::cout << "Connected to Sensei!" << std::endl;
+                        _connected = true;
+                    }
+                }
+            }
+            if (bytes < 0)
+            {
+                std::cout << "Timeout on read: " << std::endl;
+            }
+        }
+    }
+
+    void write_loop()
+    {
+        RaspaPacket buffer;
+        while (_running)
+        {
+            if (_connected)
+            {
+                /* Periodically send data to the socket */
+                snprintf(buffer.data, sizeof(buffer), "Raspa, pkt: %x", _msg_count++);
+                buffer.data[19] = 0;
+                auto ret = send(_out_socket, &buffer, sizeof(RaspaPacket), 0);
+                if (ret < sizeof(RaspaPacket))
+                {
+                    std::cout << "Failed to send with error: " << ret << std::endl;
+                }
+                else
+                {
+                    std::cout << "Sent " << ret << " bytes, pkt: " << _msg_count << std::endl;
+                }
+            }
+            std::this_thread::sleep_for(PING_INTERVAL);
+        }
+    }
+    
+    bool            _running{false};
+    bool            _connected{false};
+    std::thread     _read_thread;
+    std::thread     _write_thread;
+
+    int             _in_socket{0};
+    int             _out_socket{0}; 
+    int             _msg_count{0};
+};
+
+bool running(false);
+
+static void signal_handler(int sig_number)
+{
+    running = false;
+}
+
+int main()
+{
+    signal(SIGINT, signal_handler);
+    RaspaMockup instance;
+    instance.run();
+    running = true;
+    while(running)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+    instance.stop();
+    return 0;
+}
