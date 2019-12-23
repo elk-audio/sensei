4a58504a6dd66740e6b0bf118f2d25fc2bbfb2b7
Sharan Yagneswar <sharan.yagneswar@mindmusiclabs.com>
2019-12-23 14:06:21 +0100

    Removed old and unused files and directories

--- misc/sensei/__init__.py
+++
@@ -1,8 +0,0 @@
-# sensei module imports
-
-from sensei.sensei_packet import SenseiPacket
-from sensei.sensei_packet import SetDigitalMessage
-from sensei.sensei_packet import ConfigurePinMessage
-
-from sensei.serial_frontend import SerialFrontend
-
